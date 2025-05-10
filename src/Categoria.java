aacfdd5329c22e8a4a3329f0e9f6ce223ec92dec
Jaiverson Bonillo <jaiversonb1@gmail.com>
2025-05-10 18:41:06 -0400

    Prueba sin cambios al commit de preguntas

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+package com.trivia.modelo;
 
-public class Categoria {
+public enum Categoria {
 }
--- src/com/trivia/modelo/Categoria.java
+++ src/com/trivia/modelo/Categoria.java
@@ -1,4 +1,34 @@
 package com.trivia.modelo;
 
 public enum Categoria {
+
+    geografia("Geografia", "Azul"),
+    historia("Historia", "Amarillo"),
+    deportesPasatiempos("Deportes y pasatiempos", "Naranja"),
+    cienciasYNaturalesa("Ciencias y Naturalesa", "Verde"),
+    arteYLiteratura("Arte y Literatura", "Morado"),
+    entretenimiento("Entretenimiento", "Rosado");
+
+    private final String nombre;
+    private final String color;
+
+    Categoria(String nombre, String color){
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+    @Override
+    public String toString(){
+        return nombre + "("+ color + ")";
+    }
+
 }
